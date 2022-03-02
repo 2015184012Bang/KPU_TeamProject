6939c7f9ab16b5c9a871b3bb7a255ba768d33c3d
derisan <derisan@naver.com>
2022-03-02 12:46:03 +0900

    Update PCH

--- HeartBeat/Client/ClientPCH.h
+++ HeartBeat/Client/ClientPCH.h
@@ -1,36 +1,6 @@
 #pragma once
 
-#pragma comment(lib, "d3d12")
-#pragma comment(lib, "dxgi")
-#pragma comment(lib, "dxguid")
-#pragma comment(lib, "d3dcompiler")
-#pragma comment(lib, "DirectXTex")
-#pragma comment(lib, "DirectXTK12")
-
 #include "HeartBeat.h"
-
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#endif
-
-#include <Windows.h>
-
-#include <d3d12.h>
-#include <dxgi1_6.h>
-#include <D3Dcompiler.h>
-#include <DirectXMath.h>
-#include <DirectXPackedVector.h>
-#include <DirectXColors.h>
-#include <DirectXTK12/SimpleMath.h>
-#include <DirectXTex.h>
-#include <DirectXTex.inl>
-#include "d3dx12.h"
-
-using namespace DirectX;
-using namespace Microsoft::WRL;
-using namespace DirectX::PackedVector;
-using namespace DirectX::SimpleMath;
 
 #include "d3dHelper.h"
 #include "ClientComponents.h"
--- HeartBeat/HeartBeat/PCH.h
+++ HeartBeat/HeartBeat/PCH.h
@@ -44,3 +44,26 @@ using uint32 = unsigned __int32;
 using uint64 = unsigned __int64;
 
 namespace fs = std::filesystem;
+
+#pragma comment(lib, "d3d12")
+#pragma comment(lib, "dxgi")
+#pragma comment(lib, "dxguid")
+#pragma comment(lib, "d3dcompiler")
+#pragma comment(lib, "DirectXTex")
+#pragma comment(lib, "DirectXTK12")
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXTK12/SimpleMath.h>
+#include <DirectXTex.h>
+#include <DirectXTex.inl>
+#include "d3dx12.h"
+
+using namespace DirectX;
+using namespace Microsoft::WRL;
+using namespace DirectX::PackedVector;
+using namespace DirectX::SimpleMath;
